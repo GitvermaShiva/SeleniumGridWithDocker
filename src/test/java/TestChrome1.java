ea8f81698af472ef14c8fc7d784840fb7eb478b0
Shivam Verma <shivamverma63970@gmail.com>
2026-04-23 19:06:51 +0530

    Added test case for chrome-node1

--- src/test/java/TestChrome1.java
+++ src/test/java/TestChrome1.java
@@ -1,7 +1,24 @@
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class TestChrome1 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MalformedURLException {
 
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        DesiredCapabilities cap=new DesiredCapabilities();
+        cap.setBrowserName("chrome");
+
+        URL hubURL=new URL("http://localhost:4444/");
+
+        RemoteWebDriver driver=new RemoteWebDriver(hubURL,cap);
+
+        driver.get("https://www.google.com/");
+
+        System.out.println("Title is: "+driver.getTitle());
+
+        driver.quit();
     }
 }
