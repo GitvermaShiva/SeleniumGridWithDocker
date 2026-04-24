8f11885c047c946f22d0877cbdfd632d235f5c27
Shivam Verma <shivamverma63970@gmail.com>
2026-04-24 17:57:14 +0530

    Added the docker-compose.yaml setup

--- src/test/java/TestChrome1.java
+++ src/test/java/TestChrome1.java
@@ -1,12 +1,14 @@
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class TestChrome1 {
 
-    public static void main(String[] args) throws MalformedURLException {
+    @Test
+    public void testChrome() throws MalformedURLException {
 
         DesiredCapabilities cap=new DesiredCapabilities();
         cap.setBrowserName("chrome");
@@ -15,7 +17,7 @@ public class TestChrome1 {
 
         RemoteWebDriver driver=new RemoteWebDriver(hubURL,cap);
 
-        driver.get("https://www.google.com/");
+        driver.get("https://website-ite.uat-paytm.bank.in/");
 
         System.out.println("Title is: "+driver.getTitle());
 
